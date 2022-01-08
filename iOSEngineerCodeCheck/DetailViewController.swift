fc5ebfe40cc159bae07038aafaf2376f240bb155
KazSkt <sakata@iplab.cs.tsukuba.ac.jp>
2022-01-08 16:20:25 +0900

    watchers_countのtypo修正

--- iOSEngineerCodeCheck/DetailViewController.swift
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -28,7 +28,7 @@ class DetailViewController: UIViewController {
         
         langLabel.text = "Written in \(repository["language"] as? String ?? "")"
         starsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
-        watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        watchersLabel.text = "\(repository["watchers_count"] as? Int ?? 0) watchers"
         forksLabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
         issuesLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
